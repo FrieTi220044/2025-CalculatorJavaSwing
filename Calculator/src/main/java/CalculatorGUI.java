7ec4f33801bbc55663d59d8a16653da1ab5fe7d7
FrieTi220044 <40146720220044@litec.ac.at>
2025-03-12 14:25:48 +0100

    feat: Calculator Finished

--- Calculator/src/main/java/CalculatorGUI.java
+++ Calculator/src/main/java/CalculatorGUI.java
@@ -1,3 +1,4 @@
+import javax.management.StringValueExp;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,6 +7,13 @@ import java.awt.event.ActionListener;
 public class CalculatorGUI extends JFrame implements ActionListener {
     JButton[] numberButtons = new JButton[10];
     JButton[] operatorButtons = new JButton[6];
+    JTextField TextBar;
+    JButton negButton;
+    JButton decimalButton;
+    private double num1;
+    private double num2;
+    private char operator;
+    double result;
 
     CalculatorGUI() {
 
@@ -17,7 +25,7 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         outputPanel.setBounds(0, 0, 410, 150);
 
 
-        JTextField TextBar = new JTextField();
+        TextBar = new JTextField();
         TextBar.setBackground(Color.WHITE);
         TextBar.setBounds(75, 40, 260, 70);
         TextBar.setFont(new Font("Arial", Font.BOLD, 25)); // Größere Schrift
@@ -33,10 +41,11 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         operatorPanel.setLayout(new GridLayout(6, 1, 5, 5));
 
         // Operatoren hinzufügen
-        String[] operators = {"+", "-", "*", "/", "=", "C"};
+        String[] operators = {"+", "-", "*", "/", "=", "Clear"};
         for (int i = 0; i < operators.length; i++) {
             operatorButtons[i] = new JButton(operators[i]);
             operatorButtons[i].setFont(new Font("Arial", Font.BOLD, 24));
+            operatorButtons[i].addActionListener(this);
             operatorPanel.add(operatorButtons[i]);
         }
 
@@ -51,22 +60,26 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         for (int i = 1; i <= 9; i++) {
             numberButtons[i] = new JButton(String.valueOf(i));
             numberButtons[i].setFont(new Font("Arial", Font.BOLD, 24));
+            numberButtons[i].addActionListener(this);
             inputPanel.add(numberButtons[i]);
         }
 
         // (-) Button
-        JButton negButton = new JButton("(-)");
+        negButton = new JButton("(-)");
         negButton.setFont(new Font("Arial", Font.BOLD, 24));
+        negButton.addActionListener(this);
         inputPanel.add(negButton);
 
         // 0 Button
         numberButtons[0] = new JButton("0");
         numberButtons[0].setFont(new Font("Arial", Font.BOLD, 24));
+        numberButtons[0].addActionListener(this);
         inputPanel.add(numberButtons[0]);
 
         // . Button
-        JButton decimalButton = new JButton(".");
+        decimalButton = new JButton(".");
         decimalButton.setFont(new Font("Arial", Font.BOLD, 24));
+        decimalButton.addActionListener(this);
         inputPanel.add(decimalButton);
 
         // ---------- FRAME -------------
@@ -85,6 +98,81 @@ public class CalculatorGUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10 ; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                TextBar.setText(TextBar.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == decimalButton) {
+            TextBar.setText(TextBar.getText().concat("."));
+        }
 
+        if (e.getSource() == operatorButtons[0]) { // operatorButton[0] = +
+            num1 = Double.parseDouble(TextBar.getText());
+            operator = '+';
+            TextBar.setText("");
+        }
+
+        if (e.getSource() == operatorButtons[1]) { // operatorButton[1] = -
+            num1 = Double.parseDouble(TextBar.getText());
+            operator = '-';
+            TextBar.setText("");
+        }
+
+        if (e.getSource() == operatorButtons[2]) { // operatorButton[2] = *
+            num1 = Double.parseDouble(TextBar.getText());
+            operator = '*';
+            TextBar.setText("");
+        }
+
+        if (e.getSource() == operatorButtons[3]) { // operatorButton[3] = /
+            num1 = Double.parseDouble(TextBar.getText());
+            operator = '/';
+            TextBar.setText("");
+        }
+
+        if (e.getSource() == operatorButtons[4]) { // operatorButton[4] = =
+            num2 = Double.parseDouble(TextBar.getText());
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    TextBar.setText(String.valueOf(result));
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    TextBar.setText(String.valueOf(result));
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    TextBar.setText(String.valueOf(result));
+                    break;
+                case '/':
+                    if (num2 == 0.0) {
+                        TextBar.setText("ERROR!");
+                        break;
+                    }
+                    result = num1 / num2;
+                    TextBar.setText(String.valueOf(result));
+                    break;
+            }
+            num1 = result;
+        }
+
+        if (e.getSource() == operatorButtons[5]) {  // clear Button
+            num1 = 0;
+            num2 = 0;
+            TextBar.setText("");
+        }
+
+        if (e.getSource() == negButton) {
+            if (TextBar.getText().isEmpty()) {
+                throw new NumberFormatException("negated without a Number!");
+            }
+
+            double temp = Double.parseDouble(TextBar.getText());
+            temp *= -1;
+            TextBar.setText(String.valueOf(temp));
+        }
     }
 }
